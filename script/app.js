67faf7430ec23f889165d10852e7ba69f69c14ad
Your Name <minhphp2005@gmail.com>
2024-09-16 18:43:32 +0700

    video 04

--- script/app.js
+++ script/app.js
@@ -1,17 +1,28 @@
+function Header(props) {
+    return(
+       <div>
+        <h1 className = {props.src}>{props.title}</h1>
+        <label htmlFor=""></label>
+       </div> 
 
-const jsx  = (
-    <React.Fragment>
-    <h2>
-        Hello
-    </h2>
-    <h3>
-        Hello
-    </h3>
-    </React.Fragment>
+    )
+}
 
+const jsx = (
 
+  <React.Fragment>
+  <Header 
+    src = "đây là ảnh"
+    className = "ClassOk"
+    title = "Tôi test header"
+
+  />
+  
+    <h3>Hello</h3>
+    <h3>Hello</h3>
+  </React.Fragment>
 );
 
-const parent = document.querySelector('#hello');
+const parent = document.querySelector("#hello");
 const rootParent = ReactDOM.createRoot(parent);
 rootParent.render(jsx);
